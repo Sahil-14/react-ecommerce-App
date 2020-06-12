2c384be9897aedd5dcb6defa1ec27fbf95ca37f1
Sahil-14 <srshimpi14@gmail.com>
2020-06-12 23:06:16 +0530

    commiting collection-item component and collection-preview-component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch,Route} from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import Shoppage from './pages/shoppage/shop.comonent'
 
 const HatsPage = () => (
   <div>
@@ -16,7 +17,8 @@ function App() {
     <div>
     <Switch>
        <Route exact path='/' component={Homepage}/>
-      <Route exact path='/hats' component={HatsPage}/>
+       <Route exact path='/shop' component={Shoppage}/>
+
     </Switch>
    
 
