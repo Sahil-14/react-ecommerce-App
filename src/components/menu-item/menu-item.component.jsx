dded8f65cf0ffb53b04ca44cc0aa38d3894b1c60
Sahil-14 <srshimpi14@gmail.com>
2020-06-12 20:09:41 +0530

    update our menu-item to nevigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {withRouter} from 'react-router-dom';
 import './menuitem.componen.scss'
 
 
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div  className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size,history,linkUrl,match }) => (
+    <div 
+     className={`${size} menu-item`} 
+     onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}/>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
@@ -14,4 +16,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
+// it gives access to location,history and match
+// otherwise we can acces  it onlu through parent component
