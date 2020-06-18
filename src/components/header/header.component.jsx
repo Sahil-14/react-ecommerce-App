118433d0db57e504ab6e07dae621eb569981605f
Sahil-14 <srshimpi14@gmail.com>
2020-06-19 00:46:21 +0530

    added carticon and cartdropdown components to our application

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 // connect is higher order component that let us to modify our component have access to  to the things related to redux
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import './header.styles.scss';
-import {auth} from '../../firebase/firebase.utils'
-
-const Header = ({currentUser}) => (
+const Header = ({currentUser,hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -19,21 +20,28 @@ const Header = ({currentUser}) => (
                 Contact
             </Link>
             {
-                currentUser ?
+                currentUser ? (
                 <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
+                 ) :(
                 <Link className='option' to='/signin'>SIGN In</Link>
-            }
+                 )}
+                 <CartIcon/>
         </div>
+        {
+            hidden ? null : <CartDropdown/>
+
+        }
     </div>
 )
 
 // this state is root0reducer
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+    currentUser,
+    hidden
 })
-
+// this is nested destructuring of an object we state as a parent object from root-reducer{user:{currentUser},}
+// inside that we have user teducer and from user we are destructuring currentuser
 
 export default connect(mapStateToProps)(Header);
 
